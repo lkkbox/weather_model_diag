38317f728c2c56720a43fde4595bb319067f1209
leecw <cwleetaiwan@gmail.com>
2025-06-16 15:27:43 +0800

    update setup.sh and mjo_example.py

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
-git init
-git pull https://github.com/lkkbox/weather_model_diag
 root=$PWD
 
 cd $root/analysis
+rm pytools
 ln -s /nwpr/gfs/com120/0_tools/pytools/ .
 
+
 cd $root/analysis/modules/mjo/rmm/
+rm rmmPhaseDiagram.py RMM_Tool.py
 ln -s ~/0_tools/MJO/RMM_WH04/rmmPhaseDiagram.py ~/0_tools/MJO/RMM_WH04/RMM_Tool.py .
+
