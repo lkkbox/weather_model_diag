235fb1b85071bd7b01ca39c622ecf0b6d027337b
leecw <cwleetaiwan@gmail.com>
2025-06-23 13:32:12 +0800

    draw plots

---
+++ standalone_modules/scores/plt_scores.py
@@ -0,0 +1,160 @@
+#!/nwpr/gfs/com120/.conda/envs/rd/bin/python
+from path import PathGetter
+import pytools as pyt
+from matplotlib import pyplot as plt
+import numpy as np
+import os
+
+'''
+
+     lead1 - leadn
+var1
+var2
+var3
+ .
+ .
+ .
+
+'''
+
+
+def main():
+    region = [0, 360, -20, 20]
+    models = [
+        ('CWA_GEPSv3', 'bc', -21),
+        ('CWA_GEPSv3', 'raw', -21),
+        ('CWA_GEPSv2', 'raw', -33),
+        ('NCEP_CTRL', 'raw', 0),
+        ('NCEP_ENSAVG', 'raw', 0),
+    ]
+    variables = [
+        *[
+            (varName, lev)
+            for lev in [100, 200, 300, 500, 700, 850, 925]
+            for varName in ['u', 'v', 't']
+        ],
+        *[
+            (varName, lev)
+            for lev in [None]
+            for varName in ['u10', 'v10', 't2m', 'prec', 'mslp', 'olr']
+        ],
+    ]
+
+    models = [
+        ('CWA_GEPSv3', 'bc', -21),
+        ('CWA_GEPSv3', 'raw', -21),
+        # ('CWA_GEPSv2', 'raw', -33),
+        # ('NCEP_CTRL', 'raw', 0),
+        ('NCEP_ENSAVG', 'raw', 0),
+    ]
+    variables = [
+        *[
+            (varName, lev)
+            for lev in [200, 850]
+            for varName in ['u', 't']
+        ],
+        *[
+            (varName, lev)
+            for lev in [None]
+            for varName in ['u10', 'olr']
+        ],
+    ]
+
+    scores = readScores(models, variables, region, 'acc')
+    plot_score_card(models, variables, region, scores, 'acc')
+
+
+
+def plot_score_card(models, variables, region, scores, scoreName):
+    def plot_total_card(score, model):
+        modelName = model[0]
+        figName = f'../../../figs/op_2025q1/scores/scoreCardTotal_{modelName}.png'
+        ny, nx = score.shape
+        x = list(range(nx))
+        y = list(range(ny))
+
+        # ytick=y
+        # ytickLabel=[]
+        # for variable in variables:
+
+        fig = plt.figure()
+        ax = fig.add_subplot()
+        hsha = ax.pcolormesh(
+            x, y, score, 
+            cmap=pyt.colormaps.nclColormap('CBR_coldhot', reverse=True, numResampling=10),
+            vmin=0, vmax=1,
+        )
+        fig.colorbar(hsha)
+        fig.savefig(figName)
+
+
+    for iModel, model in enumerate(models):
+        plot_total_card(scores[iModel], model)
+
+
+
+def readScores(models, variables, region, scoreName):
+    '''
+    scores = scores[model][variable][day]
+    '''
+    initTime = pyt.tt.ymd2float(2025, 1, 1)
+    numInits = 90
+    
+
+    def read(modelName, rawOrBc, member, varName, level, scoreName):
+        print(f'reading {modelName}, {varName}, {level}, {scoreName}')
+        path = pathGetter.get_scores_path(
+            modelName, member, initTime, numInits, varName, rawOrBc
+        )
+
+        if not os.path.exists(path):
+            print(f'path not found: {path}')
+            return
+
+        if level is None:
+            minMaxs = [[None]*2, region[2:], region[:2]]
+        else:
+            minMaxs = [[None]*2, [level]*2, region[2:], region[:2]]
+
+        data, dims = pyt.nct.ncreadByDimRange(
+            path, scoreName, minMaxs, decodeTime=False
+        )
+        coslat = np.cos(dims[-2] / 180 * np.pi)[:, None]
+        data = np.nanmean(data * coslat, axis=(-1, -2)) / np.nanmean(coslat, axis=(-1, -2))
+        data= np.squeeze(data)
+        print(dims[-3])
+        print(data)
+
+        return data
+
+
+    dataDir = '../../../data'
+    pathGetter = PathGetter(dataDir)
+    initTime = pyt.tt.ymd2float(2025, 1, 1)
+
+    maxLead = 0
+    scores = []
+    for model in models:
+        modelName, rawOrBc, member = model
+        score_model = []
+        for variable in variables:
+            varName, level = variable
+            score = read(modelName, rawOrBc, member, varName, level, scoreName)
+            lead = len(score)
+            if lead > maxLead:
+                maxLead = lead
+            score_model.append(score)
+        scores.append(score_model)
+
+
+    scoresMatrix = np.nan * np.ones((len(models), len(variables), maxLead))
+    for i in range(len(models)):
+        for j in range(len(variables)):
+            score = scores[i][j]
+            scoresMatrix[i, j, :len(score)] = score
+
+    return scoresMatrix
+
+
+if __name__ == '__main__':
+    main()
